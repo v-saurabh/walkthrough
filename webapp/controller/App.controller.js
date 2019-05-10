5e920b7f3dfbe3ee7256191fa619e58a5aa80f21
saurabh vakil <saurash.vakil@gmail.com>
2019-05-10 08:07:28 +0000

    First Commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
+	"use strict";
+	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+		onOpenDialog: function () {
+			this.getOwnerComponent().openHelloDialog();
+		}
+	});
+});

f257196403c9fe7ef1b9f6b4b21930421784e5a0
saurabh vakil <saurash.vakil@gmail.com>
2019-05-13 06:00:15 +0000

    Commit 4

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -6,6 +6,11 @@ sap.ui.define([
 ], function (Controller, MessageToast, JSONModel, ResourceModel) {
 	"use strict";
 	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+
+		onInit: function () {
+			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+		},
+
 		onOpenDialog: function () {
 			this.getOwnerComponent().openHelloDialog();
 		}
